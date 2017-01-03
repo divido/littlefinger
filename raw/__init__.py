b291155ad08260bc00524303d6f09f68e2fedee5
David Diederich <divido@psu.edu>
2017-01-03 00:43:35 -0500

    Added simple service to create the database tables on demand
    This causes the database to be created in the context (i.e. username) of the web
    server, which eases future database changes

--- raw/__init__.py
+++ raw/__init__.py
@@ -7,6 +7,8 @@ from eve import Eve
 from eve_sqlalchemy import SQL
 from eve_sqlalchemy.validation import ValidatorSQL
 
+from flask import make_response
+
 from .table_base import DOMAIN, Base
 
 from .server_info import *
@@ -30,3 +32,12 @@ db = app.data.driver
 Base.metadata.bind = db.engine
 db.Model = Base
 
+@app.route('/create-db', methods=['GET'])
+def buildDatabase():
+	try:
+		db.drop_all()
+	except:
+		pass
+
+	db.create_all()
+	return make_response("", 204)
