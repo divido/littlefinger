220e3f78615e96e8574adb1f2eb358b7909ac41d
David Diederich <divido@psu.edu>
2017-01-05 00:11:34 -0500

    Simplified /accounts endpoint, splitting off update operations
    Rather than returning entries with the accounts, now the /accounts endpoint only
    gives simple information. Since unassigned entries have their own endpoint,
    there's no benefit to combining the account update with account retrieval. As
    such, the update is now its own endpoint, which can be invoked whenever desired.

--- api/accounts.py
+++ api/accounts.py
@@ -7,31 +7,43 @@ from raw import db, Account
 
 from data_import import importOfxAccounts, importOfxEntries
 
-from flask import jsonify, request
+from flask import jsonify, request, make_response
 
 # --------------------------------------------------------------------------------
 
-def expandedAccounts(update):
-	logging.info('GET /accounts, update = %s', update)
-
-	if update:
-		try:
-			importOfxAccounts(db.session)
-			importOfxEntries(db.session)
-			db.session.commit()
-
-		except Exception as e:
-			logger.error(e)
-
-	return {
-		'accounts': [item.expandedValue for item in db.session.query(Account).all()]
-	}
+def accounts():
+	"""Retrieves basic account information from the database and returns
+	in. This version does not expand to include all entries.
+	"""
+	return [item.value for item in db.session.query(Account).all()]
 
 @app.route('/accounts', methods=['GET'])
 def getAccounts():
-	"""An example API GET request demonstrating direct use of raw Database
-	classes and expanded values
+	"""REST endpoint to retrieve accounts
+	"""
+	return jsonify(accounts())
+
+# ----------------------------------------
+
+def updateAccounts():
+	"""Issues updates for all OFX accounts, downloading any new entries and
+	updating any existing ones that have changed on the server.
+	"""
+	logging.info('Updating Accounts')
+
+	try:
+		importOfxAccounts(db.session)
+		importOfxEntries(db.session)
+		db.session.commit()
+
+	except Exception as e:
+		logger.error(e)
+
+@app.route('/accounts/update', methods=['POST'])
+def postUpdate():
+	"""A slightly abused REST endpoint to initiate a update operation. This
+	returns no information, but has side effects on the database.
 	"""
 
-	update = 'update' in request.args and request.args['update']
-	return jsonify(expandedAccounts(update))
+	updateAccounts()
+	return make_response("", 204)
