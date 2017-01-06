93cb5549ac1788c8d4ac2b06ef1eecf9952bacff
David Diederich <divido@psu.edu>
2017-01-06 03:02:13 -0500

    Remove unassigned entries, since these are no longer possible

--- api/transactions.py
+++ api/transactions.py
@@ -3,39 +3,12 @@
 import logging
 
 from . import app
-from raw import db, Entry, Transaction
+from raw import db, Transaction
 
 from flask import jsonify
 from flask.json import dumps
 
 # --------------------------------------------------------------------------------
-
-def unassignedEntries():
-	"""This returns all entries that are not fully assigned to
-	transactions. This occurs when the sum of the EntrySubdivisions does not
-	equal the total value of the entry.
-	"""
-
-	# Fake this routine by returning all entries for the moment...
-	unassigned = []
-
-	entries = db.session.query(Entry).all()
-	for entry in entries:
-		unassigned.append({
-			'entry': entry.expandedValue,
-			'remainingAmount': entry.amount
-		})
-
-	return unassigned
-
-@app.route('/unassigned-entries', methods=['GET'])
-def getUnassignedEntries():
-	"""This endpoint wraps the unassignedEntries() method in JSON formatting
-	"""
-
-	return jsonify(unassignedEntries())
-
-# ----------------------------------------
 
 def unapprovedTransactions():
 	"""This returns all transactions that have not been approved by the current
