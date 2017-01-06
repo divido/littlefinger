e5e0c44897119ac4464e329f16bc3a591d40826f
David Diederich <divido@psu.edu>
2017-01-06 02:57:31 -0500

    Added service to return unapproved transactions
    Implemented as returning all transactions for the moment, since we don't have
    any approval mechanisms coded up, yet.

--- api/transactions.py
+++ api/transactions.py
@@ -3,7 +3,7 @@
 import logging
 
 from . import app
-from raw import db, Entry
+from raw import db, Entry, Transaction
 
 from flask import jsonify
 from flask.json import dumps
@@ -34,3 +34,23 @@ def getUnassignedEntries():
 	"""
 
 	return jsonify(unassignedEntries())
+
+# ----------------------------------------
+
+def unapprovedTransactions():
+	"""This returns all transactions that have not been approved by the current
+	user. These transactions need to be reviewed and modified, then marked as
+	approved to move into the next stage of the pipelen
+	"""
+
+	unapproved = []
+
+	transactions = db.session.query(Transaction).all()
+	for transaction in transactions:
+		unapproved.append(transaction.expandedValue)
+
+	return unapproved
+
+@app.route('/transactions/unapproved', methods=['GET'])
+def getUnapprovedTransactions():
+	return jsonify(unapprovedTransactions())
